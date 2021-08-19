9dbcad07420c6630235687848860796989358477
Qijie Zhu <qzhu9@ucsc.edu>
2021-08-19 13:50:18 -0700

    Created background layout and added labels.

--- MatchingGame/Form1.Designer.cs
+++ MatchingGame/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace MatchingGame
 {
-    partial class Form1
+    partial class matchingGame_Form
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,270 @@ namespace MatchingGame
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.matchingGame_TabelLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.cell1_Label = new System.Windows.Forms.Label();
+            this.cell2_Label = new System.Windows.Forms.Label();
+            this.cell3_Label = new System.Windows.Forms.Label();
+            this.cell4_Label = new System.Windows.Forms.Label();
+            this.cell5_Label = new System.Windows.Forms.Label();
+            this.cell6_Label = new System.Windows.Forms.Label();
+            this.cell7_Label = new System.Windows.Forms.Label();
+            this.cell8_Label = new System.Windows.Forms.Label();
+            this.cell9_Label = new System.Windows.Forms.Label();
+            this.cell10_Label = new System.Windows.Forms.Label();
+            this.cell11_Label = new System.Windows.Forms.Label();
+            this.cell12_Label = new System.Windows.Forms.Label();
+            this.cell13_Label = new System.Windows.Forms.Label();
+            this.cell14_Label = new System.Windows.Forms.Label();
+            this.cell15_Label = new System.Windows.Forms.Label();
+            this.cell16_Label = new System.Windows.Forms.Label();
+            this.matchingGame_TabelLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // matchingGame_TabelLayoutPanel
+            // 
+            this.matchingGame_TabelLayoutPanel.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.matchingGame_TabelLayoutPanel.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.matchingGame_TabelLayoutPanel.ColumnCount = 4;
+            this.matchingGame_TabelLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.matchingGame_TabelLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.matchingGame_TabelLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.matchingGame_TabelLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell16_Label, 3, 3);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell15_Label, 2, 3);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell14_Label, 1, 3);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell13_Label, 0, 3);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell12_Label, 3, 2);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell11_Label, 2, 2);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell10_Label, 1, 2);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell9_Label, 0, 2);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell8_Label, 3, 1);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell7_Label, 2, 1);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell6_Label, 1, 1);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell5_Label, 0, 1);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell4_Label, 3, 0);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell3_Label, 2, 0);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell2_Label, 1, 0);
+            this.matchingGame_TabelLayoutPanel.Controls.Add(this.cell1_Label, 0, 0);
+            this.matchingGame_TabelLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.matchingGame_TabelLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.matchingGame_TabelLayoutPanel.Name = "matchingGame_TabelLayoutPanel";
+            this.matchingGame_TabelLayoutPanel.RowCount = 4;
+            this.matchingGame_TabelLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.matchingGame_TabelLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.matchingGame_TabelLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.matchingGame_TabelLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.matchingGame_TabelLayoutPanel.Size = new System.Drawing.Size(534, 511);
+            this.matchingGame_TabelLayoutPanel.TabIndex = 0;
+            // 
+            // cell1_Label
+            // 
+            this.cell1_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell1_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell1_Label.Location = new System.Drawing.Point(5, 2);
+            this.cell1_Label.Name = "cell1_Label";
+            this.cell1_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell1_Label.TabIndex = 0;
+            this.cell1_Label.Text = "c";
+            this.cell1_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell2_Label
+            // 
+            this.cell2_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell2_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell2_Label.Location = new System.Drawing.Point(138, 2);
+            this.cell2_Label.Name = "cell2_Label";
+            this.cell2_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell2_Label.TabIndex = 1;
+            this.cell2_Label.Text = "c";
+            this.cell2_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell3_Label
+            // 
+            this.cell3_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell3_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell3_Label.Location = new System.Drawing.Point(271, 2);
+            this.cell3_Label.Name = "cell3_Label";
+            this.cell3_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell3_Label.TabIndex = 2;
+            this.cell3_Label.Text = "c";
+            this.cell3_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell4_Label
+            // 
+            this.cell4_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell4_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell4_Label.Location = new System.Drawing.Point(404, 2);
+            this.cell4_Label.Name = "cell4_Label";
+            this.cell4_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell4_Label.TabIndex = 3;
+            this.cell4_Label.Text = "c";
+            this.cell4_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell5_Label
+            // 
+            this.cell5_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell5_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell5_Label.Location = new System.Drawing.Point(5, 129);
+            this.cell5_Label.Name = "cell5_Label";
+            this.cell5_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell5_Label.TabIndex = 4;
+            this.cell5_Label.Text = "c";
+            this.cell5_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell6_Label
+            // 
+            this.cell6_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell6_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell6_Label.Location = new System.Drawing.Point(138, 129);
+            this.cell6_Label.Name = "cell6_Label";
+            this.cell6_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell6_Label.TabIndex = 5;
+            this.cell6_Label.Text = "c";
+            this.cell6_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell7_Label
+            // 
+            this.cell7_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell7_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell7_Label.Location = new System.Drawing.Point(271, 129);
+            this.cell7_Label.Name = "cell7_Label";
+            this.cell7_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell7_Label.TabIndex = 6;
+            this.cell7_Label.Text = "c";
+            this.cell7_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell8_Label
+            // 
+            this.cell8_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell8_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell8_Label.Location = new System.Drawing.Point(404, 129);
+            this.cell8_Label.Name = "cell8_Label";
+            this.cell8_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell8_Label.TabIndex = 7;
+            this.cell8_Label.Text = "c";
+            this.cell8_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell9_Label
+            // 
+            this.cell9_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell9_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell9_Label.Location = new System.Drawing.Point(5, 256);
+            this.cell9_Label.Name = "cell9_Label";
+            this.cell9_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell9_Label.TabIndex = 8;
+            this.cell9_Label.Text = "c";
+            this.cell9_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell10_Label
+            // 
+            this.cell10_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell10_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell10_Label.Location = new System.Drawing.Point(138, 256);
+            this.cell10_Label.Name = "cell10_Label";
+            this.cell10_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell10_Label.TabIndex = 9;
+            this.cell10_Label.Text = "c";
+            this.cell10_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell11_Label
+            // 
+            this.cell11_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell11_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell11_Label.Location = new System.Drawing.Point(271, 256);
+            this.cell11_Label.Name = "cell11_Label";
+            this.cell11_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell11_Label.TabIndex = 10;
+            this.cell11_Label.Text = "c";
+            this.cell11_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell12_Label
+            // 
+            this.cell12_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell12_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell12_Label.Location = new System.Drawing.Point(404, 256);
+            this.cell12_Label.Name = "cell12_Label";
+            this.cell12_Label.Size = new System.Drawing.Size(125, 125);
+            this.cell12_Label.TabIndex = 11;
+            this.cell12_Label.Text = "c";
+            this.cell12_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell13_Label
+            // 
+            this.cell13_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell13_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell13_Label.Location = new System.Drawing.Point(5, 383);
+            this.cell13_Label.Name = "cell13_Label";
+            this.cell13_Label.Size = new System.Drawing.Size(125, 126);
+            this.cell13_Label.TabIndex = 12;
+            this.cell13_Label.Text = "c";
+            this.cell13_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell14_Label
+            // 
+            this.cell14_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell14_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell14_Label.Location = new System.Drawing.Point(138, 383);
+            this.cell14_Label.Name = "cell14_Label";
+            this.cell14_Label.Size = new System.Drawing.Size(125, 126);
+            this.cell14_Label.TabIndex = 13;
+            this.cell14_Label.Text = "c";
+            this.cell14_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell15_Label
+            // 
+            this.cell15_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell15_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell15_Label.Location = new System.Drawing.Point(271, 383);
+            this.cell15_Label.Name = "cell15_Label";
+            this.cell15_Label.Size = new System.Drawing.Size(125, 126);
+            this.cell15_Label.TabIndex = 14;
+            this.cell15_Label.Text = "c";
+            this.cell15_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cell16_Label
+            // 
+            this.cell16_Label.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cell16_Label.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.cell16_Label.Location = new System.Drawing.Point(404, 383);
+            this.cell16_Label.Name = "cell16_Label";
+            this.cell16_Label.Size = new System.Drawing.Size(125, 126);
+            this.cell16_Label.TabIndex = 15;
+            this.cell16_Label.Text = "c";
+            this.cell16_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // matchingGame_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(534, 511);
+            this.Controls.Add(this.matchingGame_TabelLayoutPanel);
+            this.Name = "matchingGame_Form";
+            this.Text = "Matching Game";
+            this.matchingGame_TabelLayoutPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel matchingGame_TabelLayoutPanel;
+        private System.Windows.Forms.Label cell1_Label;
+        private System.Windows.Forms.Label cell16_Label;
+        private System.Windows.Forms.Label cell15_Label;
+        private System.Windows.Forms.Label cell14_Label;
+        private System.Windows.Forms.Label cell13_Label;
+        private System.Windows.Forms.Label cell12_Label;
+        private System.Windows.Forms.Label cell11_Label;
+        private System.Windows.Forms.Label cell10_Label;
+        private System.Windows.Forms.Label cell9_Label;
+        private System.Windows.Forms.Label cell8_Label;
+        private System.Windows.Forms.Label cell7_Label;
+        private System.Windows.Forms.Label cell6_Label;
+        private System.Windows.Forms.Label cell5_Label;
+        private System.Windows.Forms.Label cell4_Label;
+        private System.Windows.Forms.Label cell3_Label;
+        private System.Windows.Forms.Label cell2_Label;
     }
 }
 
--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MatchingGame
 {
-    public partial class Form1 : Form
+    public partial class matchingGame_Form : Form
     {
-        public Form1()
+        public matchingGame_Form()
         {
             InitializeComponent();
         }
